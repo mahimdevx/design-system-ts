3342049bbc96bd72a8ab9691863bbe8ee8b56a72
Mahim Farhad <mahim.devx@gmail.com>
2026-04-08 13:44:07 +1000

    fix: remove unused ref parameter in Icon component
    - Remove unused ref from function destructuring
    - forwardRef signature retained for API compatibility
    - Lucide icons don't support ref forwarding anyway
    - Resolves ESLint no-unused-vars error

--- src/components/elements/icon.tsx
+++ src/components/elements/icon.tsx
@@ -13,7 +13,7 @@ type IconProps = Omit<SVGProps<SVGSVGElement>, "ref"> &
     name: keyof typeof icons;
   };
 
-export const Icon = forwardRef<SVGSVGElement, IconProps>(function Icon({ name, size, className, ...props }, ref) {
+export const Icon = forwardRef<SVGSVGElement, IconProps>(function Icon({ name, size, className, ...props }) {
   const Component = icons?.[name];
 
   const iconClasses = iconVariants({ size, className });
@@ -21,7 +21,6 @@ export const Icon = forwardRef<SVGSVGElement, IconProps>(function Icon({ name, s
   if (!Component) return <span>{name[0]}</span>;
 
   // Lucide icons are SVG components but don't support ref forwarding
-  // The ref is provided for API compatibility but Lucide components are functional
   return <Component className={iconClasses} {...props} />;
 });
 
